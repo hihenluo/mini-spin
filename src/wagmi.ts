6cd690a52d65034a0da423d27ae10e1635adb3d3
henluo <122192113+hihenluo@users.noreply.github.com>
2025-12-17 07:28:03 +0000

    feat: Update package dependencies and add wagmi configuration

---
+++ src/wagmi.ts
@@ -0,0 +1,15 @@
+import { http, createConfig } from 'wagmi'
+import { base } from 'wagmi/chains'
+import { injected, metaMask } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [base],
+  connectors: [
+    injected(),
+    metaMask(),
+  ],
+  transports: {
+    [base.id]: http()
+    
+  },
+})
